24081df63cd8ca7ac5a4a6dc3f715b0e6b30345f
vishakha bhardwaj <vishakhabhardwaj147@gmail.com>
2024-01-09 11:34:55 +0530

    added a copyright component as a footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import Navbar from "./Navbar"
+import CopyrightBar from "./footer/CopyrightBar";
 
 function App() {
   return (
+   
+<>
    <Navbar />
+   <div>
+    {
+       <CopyrightBar />
+    }
+   </div>
+   </>
   );
 }
 
