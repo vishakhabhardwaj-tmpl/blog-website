b43cadf1a17d998e9b8575587dccc6f167d50777
vishakha bhardwaj <vishakhabhardwaj147@gmail.com>
2024-01-09 16:41:05 +0530

    Performed routing

--- src/pages/MyBlog.js
+++
@@ -1,3 +0,0 @@
-export default function MyBlog(){
-    return <h1>MyBlog</h1>
-}
---
+++ src/pages/SingleBlogPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SingleBlogPage = () => {
+  return (
+    <div>
+      single blog pages
+    </div>
+  )
+}
+
+export default SingleBlogPage
