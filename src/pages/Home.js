f70bacb7a6958588e24c8873e1d9c67575b63e33
vishakha bhardwaj <vishakhabhardwaj147@gmail.com>
2024-01-10 22:39:02 +0530

    Created a Review section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Reviews from '../components/HomePage/Reviews'
 
 const Home = () => {
   return (
     <div>
-      Hola
+        <>
+    <Reviews />
+    </>
     </div>
   )
 }
