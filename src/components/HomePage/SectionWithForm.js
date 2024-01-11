c8c287f59dd2c8f3216a537eaee2f713e37dc7ca
vishakha bhardwaj <vishakhabhardwaj147@gmail.com>
2024-01-11 14:20:25 +0530

    Newsletter subscription section created

---
+++ src/components/HomePage/SectionWithForm.js
@@ -0,0 +1,37 @@
+// SectionWithForm.js
+import React from 'react';
+import './SectionWithForm.css'; // Import your CSS file with the styles
+
+const SectionWithForm = () => {
+  return (
+    <div className="section-with-form">
+      <div className="box">
+        <h2 className="supporting-text">NEVER MISS <br></br>A NEW POST</h2>
+       
+      </div>
+      <form className="form">
+
+        <div className="form-group1">
+            <div className='form2'>
+          <label htmlFor="firstName">First Name<span className="required">*</span>:</label>
+          <input type="text" id="firstName" name="firstName" required />
+          </div>
+          <div className='form2'>
+          <label htmlFor="lastName">Last Name<span className="required">*</span>:</label>
+          <input type="text" id="lastName" name="lastName" required />
+          </div>
+          </div>
+        <div className="form-group">
+          <label htmlFor="email">Email<span className="required">*</span>
+          :</label>
+          <input type="email" id="email" name="email" required />
+        </div>
+        <div className="form-bottom">
+          <button type="submit" className="subscribe-button">Subscribe</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SectionWithForm;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Reviews from '../components/HomePage/Reviews'
+import SectionWithForm from '../components/HomePage/SectionWithForm'
+
 
 const Home = () => {
   return (
     <div>
         <>
     <Reviews />
+    <SectionWithForm/>
+    
     </>
     </div>
   )
